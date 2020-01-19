413db3930c7149730b332d75ffb010cd3c708979
Alex Rodrigues <alex.rodrigues.developer@gmail.com>
2020-01-19 00:12:15 -0300

    Initial Commit

---
+++ MapTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapTracker
+//
+//  Created by Alex Rodrigues on 1/19/20.
+//  Copyright © 2020 Alex Rodrigues. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
